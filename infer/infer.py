d8708d9c60e6e729adba1943b5f721d1ff03fc82
kamekingdom <yudainakamura2002@gmail.com>
2025-05-15 19:27:05 +0900

    Update

--- infer/infer.py
+++ infer/infer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Hiragana-YOLO evaluator
+Hiragana evaluator
   - CER, WER
   - latency (ms), per-phase speed, CPU usage (%)
   - confusion matrix + label table (PNG)
